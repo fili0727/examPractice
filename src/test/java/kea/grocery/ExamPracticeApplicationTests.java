b0a9389c00d12c652c39973c297111c4ad3e48f5
fili0727 <123998888+fili0727@users.noreply.github.com>
2024-04-12 10:20:53 +0200

    first commit

---
+++ src/test/java/kea/grocery/ExamPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.grocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
